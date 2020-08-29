be911964a3f78386129a1caf9489ab77d008be39
automobileslie <carlie.anglemire@gmail.com>
2020-08-29 17:31:55 -0400

    changed css for buttons in plant selection

--- src/ShowPlants.js
+++ src/ShowPlants.js
@@ -12,8 +12,10 @@ showThesePlants = () => {
     return  <div className = "plant-tile">
         <h2>{this.props.plant.name}</h2>
         <img className= "plant-image" src={this.props.plant.img} alt= {this.props.plant.name} onClick={()=> {this.props.pictureOnClick(this.props.plant)}}/>
+        <div className = "div-for-plant-selection-buttons">
         <button className="add-to-garden-button" onClick={() =>{this.props.buttonOnClick(this.props.plant)}}>Add to your garden</button>
         <button className= "delete-from-plant-selection-button" onClick={() =>{this.props.deleteOnClick(this.props.plant)}}>Delete this plant</button>
+        </div>
     </div>
     }
 
